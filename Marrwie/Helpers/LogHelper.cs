4725585b4c1ac771ffaa2ccbbeeb1da860e9f20a
Uygar Yilmaz <uygar.raf@gmail.com>
2020-07-10 23:46:13 -0400

    Adding a crude log helper.

---
+++ Marrwie/Helpers/LogHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Marrwie.Helpers
+{
+    public static class LogHelper
+    {
+        public static bool TryLog(string logPath, string logMessage)
+        {
+            bool result;
+
+            try
+            {
+                System.IO.File.AppendAllText(logPath, logMessage);
+                result = true;
+            }
+            catch
+            {
+                // Do nothing.
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
